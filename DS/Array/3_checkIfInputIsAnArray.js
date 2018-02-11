6c9ffc07c9f38e39b8328b274efc9e1649c5de68
Faran Shaikh <faran4engg@gmail.com>
2018-02-11 21:20:03 +0530

    check Is Array using custom logic

--- DS/Array/3_checkIfInputIsAnArray.js
+++ DS/Array/3_checkIfInputIsAnArray.js
@@ -21,14 +21,5 @@ function checkIsArray(input) {
   return 'Input passed is not an Array'
 }
 
-// solution by using in-built method of Array :)
-function is_array(word) {
-  return Array.isArray(word)
-
-}
-
-console.log(is_array('w3resource')); //f
-console.log(is_array([1, 2, 4, 0]));
-
 console.log(checkIsArray('w3resource'));
 console.log(checkIsArray([1, 2, 4, 0]));
