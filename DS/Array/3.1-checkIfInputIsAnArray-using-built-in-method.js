ed894eccfddc4d11dc40b9c941c813a3daaab278
Faran Shaikh <faran4engg@gmail.com>
2018-02-11 21:21:08 +0530

    checkIfInputIsAnArray-using-built-in-method

---
+++ DS/Array/3.1-checkIfInputIsAnArray-using-built-in-method.js
@@ -0,0 +1,8 @@
+// solution by using in-built method of Array :)
+function is_array(word) {
+  return Array.isArray(word)
+
+}
+
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
